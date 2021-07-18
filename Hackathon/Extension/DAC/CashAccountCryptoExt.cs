648aa2ed6e0f948ca269895d10a03d92edbca5b3
Nikomarov Sergey <snikomarov@acumatica.com>
2021-07-18 04:31:04 +0300

    Added links to Acumatica dlls in Site/Bin. Added an extension for CashAccount

---
+++ Hackathon/Extension/DAC/CashAccountCryptoExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PX.Data;
+using PX.Objects.CA;
+
+
+namespace Hackathon.Extension.DAC
+{
+	/// <summary>
+	/// A cash account crypto extentenstion. This class cannot be inherited.
+	/// </summary>
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public sealed class CashAccountCryptoExt : PXCacheExtension<CashAccount>
+	{
+
+	}
+}

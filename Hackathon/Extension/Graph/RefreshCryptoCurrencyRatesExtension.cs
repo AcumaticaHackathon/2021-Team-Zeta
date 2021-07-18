5615aaa14c1e797301ad47f71cd00d95bdcefb6a
Nikomarov Sergey <snikomarov@acumatica.com>
2021-07-18 07:13:13 +0300

    added RefreshCryptoCurrencyRatesExtension graph extension

---
+++ Hackathon/Extension/Graph/RefreshCryptoCurrencyRatesExtension.cs
@@ -0,0 +1,14 @@
+﻿using PX.Data;
+using PX.Objects.CA;
+using PX.Objects.CM;
+
+using System.Collections;
+
+namespace Hackathon
+{
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public class RefreshCryptoCurrencyRatesExtension : PXGraphExtension<RefreshCurrencyRates>
+    {
+		
+	}
+}

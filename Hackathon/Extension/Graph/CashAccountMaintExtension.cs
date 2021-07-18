edf35b975695288adef9a3a5ddf930f851b74dff
Joshua Van Hoesen <jvanhoesen@asillc.com>
2021-07-18 15:38:29 -0400

    code update

--- Hackathon/Extension/Graph/CashAccountMaintExtension.cs
+++ Hackathon/Extension/Graph/CashAccountMaintExtension.cs
@@ -1,79 +1,140 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-
 using PX.Data;
 using PX.Objects.CA;
 using System.Collections;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace Hackathon
 {
-	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
-	public class CashAccountMaintExtension : PXGraphExtension<CashAccountMaint>
+    // Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+    public class CashAccountMaintExtension : PXGraphExtension<CashAccountMaint>
     {
-		#region CheckBalance
+        #region CheckBalance
 
-		public PXAction<CashAccount> CheckBalance;
+        public PXAction<CashAccount> CheckBalance;
 
-		public override void Initialize()
-		{
-			AppDomain.CurrentDomain.AssemblyResolve += RestSharpAssemblyResolve;
-		}
+        public override void Initialize()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += RestSharpAssemblyResolve;
+        }
 
-		private Assembly RestSharpAssemblyResolve(object sender, ResolveEventArgs args)
-		{
-			if (args.Name != null && args.Name.Contains("RestSharp"))
-			{
-				string pxDataLocation = typeof(PXGraph).Assembly.CodeBase;
-				
-				if (Uri.IsWellFormedUriString(pxDataLocation, UriKind.Absolute))
-				{
-					Uri pxDataUri = new Uri(pxDataLocation);
-					pxDataLocation = pxDataUri.LocalPath;
-				}
-
-				string binDir = Path.GetDirectoryName(pxDataLocation);
-				string restSharp = Path.Combine(binDir, "RestSharp.dll");
-
-				if (!File.Exists(restSharp))
-					return null;
-
-				try
-				{
-					var restSharpAssembly = Assembly.LoadFrom(restSharp);
-					return restSharpAssembly;
-				}
-				catch (Exception)
-				{
-					return null;
-				}
-			}
-
-			return null;
-		}
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="adapter"></param>
-		/// <returns></returns>
-		[PXButton]
-		[PXUIField(DisplayName = "Check Balance", MapEnableRights = PXCacheRights.Update, MapViewRights = PXCacheRights.Update)]
-		protected virtual IEnumerable checkBalance(PXAdapter adapter)
-		{
-			CashAccount account = Base.CashAccount.Current;
-			if(account != null)
+        private Assembly RestSharpAssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            if (args.Name != null && args.Name.Contains("RestSharp"))
             {
-				CashAccountCryptoExt accountExt = PXCache<CashAccount>.GetExtension<CashAccountCryptoExt>(account);
+                string pxDataLocation = typeof(PXGraph).Assembly.CodeBase;
 
-				BlockSharp.BlockSharp client = new BlockSharp.BlockSharp(accountExt.UsrTZAPIKey);
+                if (Uri.IsWellFormedUriString(pxDataLocation, UriKind.Absolute))
+                {
+                    Uri pxDataUri = new Uri(pxDataLocation);
+                    pxDataLocation = pxDataUri.LocalPath;
+                }
 
-				var result = client.GetBalance();
+                string binDir = Path.GetDirectoryName(pxDataLocation);
+                string restSharp = Path.Combine(binDir, "RestSharp.dll");
+
+                if (!File.Exists(restSharp))
+                    return null;
+
+                try
+                {
+                    var restSharpAssembly = Assembly.LoadFrom(restSharp);
+                    return restSharpAssembly;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
             }
 
-			return adapter.Get();
-		}
+            return null;
+        }
 
-		#endregion
-	}
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="adapter"></param>
+        /// <returns></returns>
+        [PXButton]
+        [PXUIField(DisplayName = "Check Balance", MapEnableRights = PXCacheRights.Update, MapViewRights = PXCacheRights.Update)]
+        protected virtual IEnumerable checkBalance(PXAdapter adapter)
+        {
+            CashAccount account = Base.CashAccount.Current;
+            if (account != null)
+            {
+                CashAccountCryptoExt accountExt = PXCache<CashAccount>.GetExtension<CashAccountCryptoExt>(account);
+
+                BlockSharp.BlockSharp client = new BlockSharp.BlockSharp(accountExt.UsrTZAPIKey);
+
+                var result = client.GetBalance();
+
+                throw new Exception(result.AvailableBalance + " Doge coin available");
+            }
+
+            return adapter.Get();
+        }
+
+        public PXAction<CashAccount> MakeTransfer;
+
+        [PXButton]
+        [PXUIField(DisplayName = "Make Transfer", MapEnableRights = PXCacheRights.Update, MapViewRights = PXCacheRights.Update)]
+        protected virtual IEnumerable makeTransfer(PXAdapter adapter)
+        {
+            CashAccount account = Base.CashAccount.Current;
+            if (account != null)
+            {
+                WebRequest webRequest = WebRequest.Create(@"https://block.io/api/v2/prepare_transaction/?api_key=1fb1-ce42-104a-dd17&from_addresses=2Myzgr5pCrBLQ8zr6S7SRbsoAUs6zv5ew89&to_address=2MzPUSrjkauaWtuzGV9RYm6pi4SfygEhHBL&amounts=10");
+                WebResponse webResp = webRequest.GetResponse();
+
+                String responseString = string.Empty;
+                string responseHex = string.Empty;
+                using (Stream stream = webResp.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                    responseString = reader.ReadToEnd();
+                    responseHex = BlockSharp.Core.Crypto.ASCIIToHex(responseString);
+                }
+
+                PrepareResponse response = JsonConvert.DeserializeObject<PrepareResponse>(responseString);
+
+
+                SubmitRequest request = new SubmitRequest();
+
+                request.tx_type = response.data.tx_type;
+                request.tx_hex = responseHex;
+
+                Signature sig = new Signature();
+                foreach (InputAddressData addressData in response.data.input_address_data)
+                {
+                    int index = 0;
+                    foreach (string publicKey in addressData.public_keys)
+                    {
+                        sig.input_index = index;
+                        sig.public_key = publicKey;
+                        request.signatures.Add(sig);
+                        index++;
+                    }
+                }
+
+                string result = Newtonsoft.Json.JsonConvert.SerializeObject(request);
+
+                webRequest = WebRequest.Create(@"https://block.io/api/v2/submit_transaction/?api_key=1fb1-ce42-104a-dd17 -d " + result + " -H 'Content-Type: application/json'");
+                webResp = webRequest.GetResponse();
+
+                responseString = string.Empty;
+                using (Stream stream = webResp.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                    responseString = reader.ReadToEnd();
+                }
+            }
+            return adapter.Get();
+        }
+
+        #endregion
+    }
 }
---
+++ Hackathon/SubmitRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon
+{
+    public class Signature
+    {
+        public int input_index { get; set; }
+        public string public_key { get; set; }
+        public string signature { get; set; }
+    }
+
+    public class SubmitRequest
+    {
+        public string tx_type { get; set; }
+        public string tx_hex { get; set; }
+        public IList<Signature> signatures { get; set; }
+    }
+
+}

91badfbe4757cc087d9737ea67d68059aac7fe9a
Nikomarov Sergey <snikomarov@acumatica.com>
2021-07-18 08:55:01 +0300

    added currency rate type extension some work on polling

---
+++ Hackathon/Extension/DAC/CurrencyRateTypeCryptoExt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PX.Data;
+using PX.Data.BQL;
+using PX.Objects.CM;
+
+namespace Hackathon
+{
+	
+	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
+	public sealed class CurrencyRateTypeCryptoExt : PXCacheExtension<CurrencyRateType>
+	{
+		#region UsrTZAPIKey
+		public abstract class isCryptoCurrency : BqlBool.Field<isCryptoCurrency>
+		{
+		}
+		[PXDBBool()]
+		[PXDefault(false)]
+		[PXUIField(DisplayName = "Is Crypto Currency")]
+		public bool? IsCryptoCurrency { get; set; }
+		#endregion
+	}
+}
--- Hackathon/Extension/Graph/RefreshCryptoCurrencyRatesExtension.cs
+++ Hackathon/Extension/Graph/RefreshCryptoCurrencyRatesExtension.cs
@@ -1,4 +1,8 @@
-﻿using PX.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using PX.Data;
 using PX.Objects.CA;
 using PX.Objects.CM;
 
@@ -9,6 +13,11 @@ namespace Hackathon
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
 	public class RefreshCryptoCurrencyRatesExtension : PXGraphExtension<RefreshCurrencyRates>
     {
-		
+		private const string ApiKey = "1fb1-ce42-104a-dd17";
+
+		[PXOverride]
+		public virtual string GetApiKey(Func<string> baseMethod) => ApiKey;
+
+
 	}
 }

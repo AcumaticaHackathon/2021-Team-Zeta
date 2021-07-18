a66dc4091ae77aceda58b3266c6cec5cd21b0a88
Nikomarov Sergey <snikomarov@acumatica.com>
2021-07-18 10:51:38 +0300

    added usr prefix to the field's name

--- Hackathon/Extension/DAC/RefreshRateCryptoExt.cs
+++ Hackathon/Extension/DAC/RefreshRateCryptoExt.cs
@@ -10,17 +10,15 @@ using PX.Objects.CM;
 
 namespace Hackathon
 {
-	
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
-	public sealed class CurrencyRateTypeCryptoExt : PXCacheExtension<CurrencyRateType>
+	public sealed class RefreshRateCryptoExt : PXCacheExtension<RefreshRate>
 	{
 		#region UsrIsCryptoCurrency
 		public abstract class usrIsCryptoCurrency : BqlBool.Field<usrIsCryptoCurrency>
 		{
 		}
 
-		[PXDBBool()]
-		[PXDefault(false)]
+		[PXBool]
 		[PXUIField(DisplayName = "Is Crypto Currency")]
 		public bool? UsrIsCryptoCurrency { get; set; }
 		#endregion
--- Hackathon/Extension/Graph/RefreshCryptoCurrencyRatesExtension.cs
+++ Hackathon/Extension/Graph/RefreshCryptoCurrencyRatesExtension.cs
@@ -13,11 +13,15 @@ namespace Hackathon
 	// Acuminator disable once PX1016 ExtensionDoesNotDeclareIsActiveMethod extension should be constantly active
 	public class RefreshCryptoCurrencyRatesExtension : PXGraphExtension<RefreshCurrencyRates>
     {
-		private const string ApiKey = "1fb1-ce42-104a-dd17";
+		//private const string ApiKey = "1fb1-ce42-104a-dd17";
+
+		//[PXOverride]
+		//public virtual string GetApiKey(Func<string> baseMethod) => ApiKey;
 
 		[PXOverride]
-		public virtual string GetApiKey(Func<string> baseMethod) => ApiKey;
-
-
+		public virtual IEnumerable currencyRateList(Func<IEnumerable> baseDelegate)
+		{ 
+			var 
+		}
 	}
 }
